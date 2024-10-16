dc92626a11710fb602d34a70e6aeffdc18016f1c
Prajakta Naik <prajaktanaik98@gmail.com>
2024-10-16 17:05:36 +0530

    new project setup(second time)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2 className='text-3xl font-bold underline text-slate-400'>URL SHORTNER</h2>
+    </>
+  )
+}
+
+export default App
