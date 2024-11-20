d33e880becc110c45bf3b82c1001ce81aaa7fe38
Prajakta Naik <prajaktanaik98@gmail.com>
2024-11-20 16:44:49 +0530

    backend setup

---
+++ Backend/src/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const bycryptjs = require("bycryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//pre-save hook to hash password
+//this is a mongoose pre-save middelware function
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
---
+++ Backend/src/routes/authRoutes.js
@@ -0,0 +1,15 @@
+const User = require("../models/User");
+
+const registerUser = async () => {
+  const { username, email, password } = req.body;
+  try {
+    const user = new User({ username, email, password });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  }
+};
+
+
+
