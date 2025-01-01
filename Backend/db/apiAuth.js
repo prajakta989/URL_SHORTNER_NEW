ba127b6dd557b9488c7029e90485404414b3e226
Prajakta Naik <prajaktanaik98@gmail.com>
2025-01-01 12:51:10 +0530

    api call for login functionality

--- Backend/db/apiAuth.js
+++ Backend/db/apiAuth.js
@@ -1,12 +1,26 @@
 export async function login({ email, password }) {
-  const { data, error } = await supabase.auth.signInWithPassword({
-    email,
-    password,
-  });
+  // const { data, error } = await supabase.auth.signInWithPassword({
+  //   email,
+  //   password,
+  // });
 
-  if (error) throw new Error(error.message);
+  // if (error) throw new Error(error.message);
 
-  return data;
+  // return data;
+  const url = "http://localhost:3000/api/auth/login";
+  try{
+    const response = await fetch(url, {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify({email, password})
+    })
+    const result = await response.json();
+    return result;
+  }catch(error){
+    throw new Error(error.message);
+  }
 }
 
 export async function signup({ username, email, password }) {
